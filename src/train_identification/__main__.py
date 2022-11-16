3470128046f710d2f7c77c3741a2fe41765e596a
svbogdanov <sb104@yandex.ru>
2022-11-16 16:08:53 +0100

    Checkpoint filename added

--- src/train_identification/__main__.py
+++ src/train_identification/__main__.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     datamodule = DataModulePL("./data")
     model = ModulePL(datamodule.base_dataset.num_classes)
     trainer = Trainer(
-        callbacks=[ModelCheckpoint(dirpath="./checkpoints/FaceID", monitor="val_loss")],
+        callbacks=[ModelCheckpoint(
+            dirpath="./checkpoints/FaceID",
+            monitor="val_loss",
+            filename="{epoch}-{val_loss:.2f}"
+        )],
         logger=WandbLogger(project="INF634_FaceId"),
         num_sanity_val_steps=2,
         log_every_n_steps=10,
