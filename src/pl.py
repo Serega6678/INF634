af7a83881b779105824bc51cfbfdbf07bd033b3d
svbogdanov <sb104@yandex.ru>
2022-11-23 15:29:46 +0100

    test loss and metrics calculation implemented

--- src/pl.py
+++ src/pl.py
@@ -21,8 +21,10 @@ class ModulePL(pl.LightningModule):
 
         self.train_acc = Accuracy()
         self.val_acc = Accuracy()
+        self.test_acc = Accuracy()
         self.train_roc_auc = AUROC(num_classes=out_dim)
         self.val_roc_auc = AUROC(num_classes=out_dim)
+        self.test_roc_auc = AUROC(num_classes=out_dim)
 
     def forward(self, imgs: torch.Tensor) -> tp.Any:
         return self.model(imgs)
@@ -58,6 +60,21 @@ class ModulePL(pl.LightningModule):
         self.log("val_accuracy_epoch", self.val_acc)
         self.log("val_rocauc_epoch", self.val_roc_auc)
 
+    def test_step(self, test_batch: tp.Dict[str, tp.Any], batch_idx: tp.Any) -> None:
+        imgs = test_batch["img"]
+        target = test_batch["face_idx"]
+        output = self.forward(imgs)
+        loss = self.criterion(output, target)
+        preds = output.argmax(-1)
+        self.test_acc(preds, target)
+        self.test_roc_auc(output, target)
+        self.log("test_loss", loss)
+        self.log("test_accuracy", self.test_acc)
+
+    def test_epoch_end(self, outputs) -> None:
+        self.log("test_accuracy_epoch", self.test_acc)
+        self.log("test_rocauc_epoch", self.test_roc_auc)
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         scheduler = MultiStepLR(optimizer, [8, 14], 0.1, verbose=True)
@@ -78,16 +95,24 @@ class DataModulePL(pl.LightningDataModule):
 
         self.train_dataset = None
         self.val_dataset = None
+        self.test_dataset = None
 
         self.batch_size = batch_size
 
     def setup(self, stage: str) -> None:
-        train_size = len(self.base_dataset) // 4 * 3
-        val_size = len(self.base_dataset) - train_size
-        self.train_dataset, self.val_dataset = random_split(self.base_dataset, [train_size, val_size])
+        train_size = int(len(self.base_dataset) * 0.6)
+        val_size = (len(self.base_dataset) - train_size) // 2
+        test_size = len(self.base_dataset) - train_size - val_size
+        assert train_size + val_size + test_size == len(self.base_dataset)
+        self.train_dataset, self.val_dataset, self.test_dataset = random_split(
+            self.base_dataset, [train_size, val_size, test_size]
+        )
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=8)
 
     def val_dataloader(self) -> DataLoader:
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=8)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=8)
--- src/train_identification/__main__.py
+++ src/train_identification/__main__.py
@@ -1,10 +1,11 @@
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.loggers import WandbLogger
 
 from src.pl import ModulePL, DataModulePL
 
 if __name__ == "__main__":
+    seed_everything(42)
     datamodule = DataModulePL("./data")
     print("Number of classes:", datamodule.base_dataset.num_classes)
     model = ModulePL(datamodule.base_dataset.num_classes)
